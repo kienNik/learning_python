377af027bb54f66cfcc9b5b9e15f38cc88e25691
kientt <kientrantrung.hanoi@gmail.com>
2025-05-13 23:02:51 +0700

    snake game: optimize animation when init food

--- udemy_complete_python_pro_bootcamp/snake_game/food.py
+++ udemy_complete_python_pro_bootcamp/snake_game/food.py
@@ -4,11 +4,12 @@ from random import randrange
 class Food:
     def __init__(self, screen_size):
         self.coordinate_limit = screen_size/2
-        self.food = Turtle(shape='circle')
+        self.food = Turtle(shape='circle', visible=False)
         self.food.penup()
         random_x = randrange(-self.coordinate_limit+20, self.coordinate_limit-20)
         random_y = randrange(-self.coordinate_limit+20, self.coordinate_limit-20)
         self.food.setpos((random_x, random_y))
+        self.food.showturtle()
 
     def reinit(self):
         random_x = randrange(-self.coordinate_limit+20, self.coordinate_limit-20)
